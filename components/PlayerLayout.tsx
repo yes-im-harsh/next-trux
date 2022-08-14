9c5070558a746abbcf7ba96dfa29a8a1aeb4ee18
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-08-15 02:40:36 +0530

    Adding the logo

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -1,13 +1,14 @@
 import { Box } from "@chakra-ui/layout";
+import Sidebar from "./sidebar";
 
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" left="0" width="250px">
-        Side Bar
+        <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
-        Layout {children}
+        {children}
       </Box>
       <Box position="absolute" bottom="0" left="0">
         Player
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "reset-css";
-import PlayerLayout from "../components/PlayerLayout";
+import PlayerLayout from "../components/playerLayout";
 
 const theme = extendTheme({
   colors: {
