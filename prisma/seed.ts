85e6eb5583e0a310d3f65c36d85fb0675ae858c6
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-08-16 23:08:09 +0530

    Seeding Data & Running the Seed Script

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { artistsData } from "./songsData";
+
+//Connects to database
+const prisma = new PrismaClient();
+
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            })),
+          },
+        },
+      });
+    })
+  );
+};
+
+run()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
