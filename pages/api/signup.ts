39296a4ef1c786cc700eb2f64cc745d233edebb7
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-08-18 23:24:29 +0530

    Serverless API & SignUp API Route

---
+++ pages/api/signup.ts
@@ -0,0 +1,48 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
+
+// Serverless Function are just like callbackfunction, when a event(in this case new route) occurs it get triggered, and shut dows after few Time. So it's like a callback function on cloud, that why its serverless.
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync();
+  const { email, password } = req.body;
+
+  let user;
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (error) {
+    res.status(401);
+    res.json({ error: "User already exists" });
+    return;
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    "hello",
+    { expiresIn: "8h" }
+  );
+
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("TRUX_ACCESS_TOKEN", token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    })
+  );
+};
