c64f172a3bee05f83a344fc1c44d846eb300dda1
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-08-19 01:59:56 +0530

    Auth Pages

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,4 +1,4 @@
-export default fetcher = (url: string, data: undefined) => {
+export default fetcher = (url: string, data = undefined) => {
   return fetch(`${window.location.origin}/api/${url}`, {
     method: data ? "POST" : "GET",
     credentials: "include",
